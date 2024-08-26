45fa75d0fa9db8eae515db12fd1fadd4816211ae
nvanschaack <nellvanschaack@gmail.com>
2024-08-26 10:51:24 -0500

    render update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cors())
 
 
 app.get("/", (req, res) => res.send("Express on Vercel"));

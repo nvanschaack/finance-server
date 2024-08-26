f2871e459fa45bb6f5101c1a95490cc1e26ada06
nvanschaack <nellvanschaack@gmail.com>
2024-08-26 10:20:47 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 require('dotenv').config()
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
